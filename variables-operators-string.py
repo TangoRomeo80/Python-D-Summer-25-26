a7eab4cb123406a55247c599a650f94454488fc6
Tanzeem Rahat <75111696+TangoRomeo80@users.noreply.github.com>
2026-06-15 22:42:03 +0600

    Add files via upload

---
+++ variables-operators-string.py
@@ -0,0 +1,59 @@
+# Arithmetic Operators
+# a = 10
+# b = 3
+
+# print("Addition:", a + b)
+# print("Subtraction:", a - b)
+# print("Multiplication:", a * b)
+# print("Float Division:", a / b)
+# print("Integer Division:", a // b)
+# print("Modulus:", a % b)
+# print("Exponentiation:", a ** b)
+
+# a = 10
+# b = 20
+# c = 15
+# print((a + b + c) / 3)
+
+# a = "1"
+# b = "1"
+# print(a + b)
+
+# Logical Operators
+# has_id = True
+# is_registered = True
+
+# can_enter_campus = has_id and is_registered
+# print("Can enter campus:", can_enter_campus)
+
+# is_weekend = True
+# is_holiday = False
+# class_e_jabo_na = is_weekend or is_holiday
+# print("Class e jabo na:", class_e_jabo_na)
+
+# is_raining = False
+# print("Will go outside:", not is_raining)
+
+# Comparison Operators
+# a = 10
+# y = 20
+# print("a == y:", a == y)
+# print("a != y:", a != y)
+# print("a > y:", a > y)
+# print("a < y:", a < y)
+# print("a >= y:", a >= y)
+# print("a <= y:", a <= y)
+
+# a = "1"
+# b = "1"
+# print(a + b)  # String concatenation
+# print(int(a) + int(b))  # Convert strings to integers and add
+
+# name = "Alice"
+# print(f"Hello, {name}!")
+
+# price = 99.92645668977
+# print(f"Price: {price:.2f}")
+
+# print("Hello", end=", ")
+# print("World!")
