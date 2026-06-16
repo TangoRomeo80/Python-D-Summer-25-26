ef201574dc3b564e219b44bde57c443449acd7fb
Tanzeem Rahat <75111696+TangoRomeo80@users.noreply.github.com>
2026-06-16 09:19:34 +0600

    Create mutability-immutability.py

---
+++ mutability-immutability.py
@@ -0,0 +1,52 @@
+# Immutable datatypes: 
+## Numeric: int, float, complex
+## Sequence: str, tuple, range
+# Mutable datatypes:
+## Sequence: list
+## Set: set
+## Collection: dict
+
+# a= 10
+# print(id(a))
+# a = a + 2
+# print(id(a))
+# print(a)
+
+# text = "Hello"
+# text = text + " World"
+# print(text)
+
+# a = [1, 2, 3]
+# a.append(4)
+# print(a)
+
+# x = 10
+# y = x
+# print(x)
+# print(y)
+# x = 20
+# print(x)
+# print(y)
+
+# a = [1, 2, 3]
+# b = a
+# print(id(a))
+# print(id(b))
+# print(a)
+# print(b)
+# a.append(4)
+# print(a)
+# print(b)
+
+# a = [1, 2, 3]
+# b = a.copy()
+# print(id(a))
+# print(id(b))
+
+# tuple_a = (1, 2, [3, 4])
+# tuple_a[2].append(5)
+# print(tuple_a)
+# print(tuple_a[2][2])
+
+
+
