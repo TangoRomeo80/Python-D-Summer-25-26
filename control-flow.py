d793464250a9755b1969df0809162a766d8ea4dd
Tanzeem Rahat <75111696+TangoRomeo80@users.noreply.github.com>
2026-06-16 09:59:57 +0600

    Create control-flow.py

---
+++ control-flow.py
@@ -0,0 +1,53 @@
+# marks = 0
+
+# if marks:
+#     print("Pass")
+# elif marks >=90:
+#     print("A+")
+# else:
+#     print("Fail")
+
+# is_raining = False
+# is_moroccan = False
+
+# if is_raining:
+#     print("Take an umbrella")
+#     if is_moroccan:
+#         print("Wear a djellaba")
+# else:
+#     print("Enjoy the sunshine")
+
+# x = int(input("Enter a number: "))
+# print(x + 1)
+
+# day = 3
+# match day:
+#     # case 1 | 2 | 3 | 4 | 5:
+#     case 1:
+#         print("Monday")
+#     case 2:
+#         print("Tuesday")
+#     case 3:
+#         print("Wednesday")
+#     case 4:
+#         print("Thursday")
+#     case 5:
+#         print("Friday")
+#     case 6:
+#         print("Saturday")
+#     case 7:
+#         print("Sunday") 
+#     case _:
+#         print("Invalid day")
+
+# x = 10
+# while x > 0:
+#     print(x)
+#     x -= 1
+
+# Pera is in for loop
+
+# for i in range(5):
+#     print(i)
+
+print(list(range(5)))
