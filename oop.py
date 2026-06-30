997bbaeac014797baf846b23bc947f128619b869
Tanzeem Rahat <75111696+TangoRomeo80@users.noreply.github.com>
2026-06-30 09:52:52 +0600

    Create oop.py

---
+++ oop.py
@@ -0,0 +1,7 @@
+# This file contains the implementation of object-oriented programming concepts in Python.
+class Animal:
+    def make_sound(self):
+        print('Sound')
+
+a = Animal()
+a.make_sound()  # Output: Sound
